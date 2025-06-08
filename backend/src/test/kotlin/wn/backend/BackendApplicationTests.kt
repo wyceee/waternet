0ecc5401c0bf5134db45737663522705123761b9
YC <yusuf.calik@hva.nl>
2025-06-08 16:29:03 +0200

    Update project configuration for backend module and Java 21 support

--- backend/src/test/kotlin/wn/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/wn/backend/BackendApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class BackendApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+	@Test
+	fun contextLoads() {
+	}
 
 }
